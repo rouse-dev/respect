a0d49585166c1d3b4bc5cd09fbe660827d18b12a
woookle <r1ngobtw@yandex.ru>
2025-01-22 21:23:47 +0300

    little update
    * Update Home.tsx
    * Update Profile.tsx

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import placeholder_avatar from '../assets/media/placeholder_avatar.png';
+import placeholder_avatar from '../assets/media/placeholder_avatar.png'
 import { Link } from "react-router-dom";
 
 const Profile = () => {
@@ -10,8 +10,12 @@ const Profile = () => {
 
     return (
         <div className="max-w-6xl mx-auto flex flex-col items-center gap-5 text-lg p-3 sm:p-5 text-white">
-            <Link className="flex flex-row justify-self-end w-full sm:w-fit justify-center items-center sm:ml-auto mb-3 sm:mb-12 px-3 py-2 rounded-lg gap-4 bg-[--respect-purple-deep]" to='/'>На главную</Link>
-            <div className="flex flex-col items-center gap-3 border-4 border-[--respect-purple-dark] w-full max-w-md p-5 bg-[--respect-purple-deep] rounded-lg">
+            <div className="w-full flex flex-row justify-between sm:mb-12">
+                <button className="hidden sm:block w-full sm:w-fit px-3 py-2 rounded-lg bg-[--respect-purple-deep]">Выход</button>
+                <Link className="w-full sm:w-fit px-3 py-2 rounded-lg bg-[--respect-purple-deep]" to='/'>На главную</Link>
+            </div>
+            
+            <div className="flex flex-col items-center gap-3 w-full sm:max-w-md p-5 bg-[--respect-purple-deep] rounded-lg">
                 <img className="max-w-28 max-h-28 w-full h-full rounded-[100%]" src={user.avatar ? `http://localhost:0000/some_folder/${user.avatar}` : placeholder_avatar} />
                 <input className="hidden" type="file" id="avatar_file" accept="image/*" />
                 <button className="bg-[--respect-purple-dark] mb-2 px-3 py-2 rounded-lg" onClick={e => {
@@ -19,12 +23,13 @@ const Profile = () => {
                     input.click();
                 }}>Загрузить файл</button>
 
-                <input className="border-4 border-[--respect-purple-dark] bg-transparent w-full px-3 py-2 rounded-lg outline-none" placeholder="Имя" type="text" required />
-                <input className="border-4 border-[--respect-purple-dark] bg-transparent w-full px-3 py-2 rounded-lg outline-none" placeholder="Эл. почта" type="email" required />
-                <input className="border-4 border-[--respect-purple-dark] bg-transparent w-full px-3 py-2 rounded-lg outline-none" placeholder="Пароль" type="password" required />
+                <input className="bg-[--respect-purple-light] w-full px-3 py-2 rounded-lg outline-none" placeholder="Имя" type="text" required />
+                <input className="bg-[--respect-purple-light] w-full px-3 py-2 rounded-lg outline-none" placeholder="Эл. почта" type="email" required />
+                <input className="bg-[--respect-purple-light] w-full px-3 py-2 rounded-lg outline-none" placeholder="Пароль" type="password" required />
 
                 <button className="bg-[--respect-purple-dark] w-full mt-2 px-2 py-4 rounded-lg">Сохранить изменения</button>
             </div>
+            <button className="sm:hidden w-full mb-3 sm:mb-12 px-3 py-2 rounded-lg bg-[--respect-purple-deep]">Выход</button>
         </div>
     )
 }
